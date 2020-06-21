1baa0d06fe439c7a5e8c5b92e6a6a075ac5632e3
Shaun Schwartz <me@shaunschwartz.com>
2020-06-21 13:19:43 -0700

    add es search function

--- app/models/ip_address_range.rb
+++ app/models/ip_address_range.rb
@@ -9,4 +9,39 @@ class IpAddressRange < ApplicationRecord
   end
 
   UPSERTABLE_COLUMNS = [:country_id, :start_ip, :end_ip, :start_int, :end_int, :mask]
+
+  def self.search(ip)
+    res = elastic_search(ip)
+    country_id = res.first.country_id
+    return '' unless country_id
+    Country.find(country_id)
+  end
+
+  def self.elastic_search(ip)
+    ip_int = Utils.ip_to_int(ip)
+    @search_definition = {
+      size: 1,
+      query: {
+        bool: {
+          must: [
+            {
+              range: {
+                start_int: {
+                  lte: ip_int
+                }
+              }
+            },
+            {
+              range: {
+                end_int: {
+                  gte: ip_int
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+    __elasticsearch__.search(@search_definition)
+  end
 end
