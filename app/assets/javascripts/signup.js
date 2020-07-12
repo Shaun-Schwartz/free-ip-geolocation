c29a0d472c9d1e40aa233c5aee92dedfc19bc22f
Shaun Schwartz <me@shaunschwartz.com>
2020-07-12 12:05:43 -0700

    remove signup jquery for now

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-  var password = $('#password').val()
-  var confirmPassword = $('#confirm-password').val()
-  $('#confirm-password').change(function() {
-    if (password.length <= 6) return
-    if (password === confirmPassword)
-      console.log("YES")
-  })
-});
+// $(document).ready(function() {
+//   console.log('ready')
+//   $('#confirm-password').change(function() {
+//     console.log('change')
+//     var password = $('#password').val()
+//     var confirmPassword = $('#confirm-password').val()
+//     console.log('confirm-password')
+//     if (password.length <= 6) return
+//     if (password === confirmPassword) {
+//       $('#confirm-password').prop( "disabled", false );
+//     }
+//   })
+// });
