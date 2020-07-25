718eee7b0fe707958087678c645fc415fd6a8690
Shaun Schwartz <me@shaunschwartz.com>
2020-07-25 06:58:13 -0700

    remove ability to pass IP address array

--- app/controllers/api/ip_addresses_controller.rb
+++ app/controllers/api/ip_addresses_controller.rb
@@ -7,28 +7,12 @@ class Api::IpAddressesController < Api::ApplicationController
     if @single_ip
       location = IpAddressRange.search(@single_ip)
       render json: LocationSerializer.new(location)
-    elsif @ip_array
-      locations = @ip_array.map do |ip|
-        IpAddressRange.search(ip)
-      end
-      if locations
-        render json: LocationSerializer.new(locations).serializable_hash
-      end
     end
   end
 
   private
   def validate_ip_params
     @single_ip = params[:ip] if params[:ip]
-    if params[:ips]
-      @ip_array = Utils::Collection.to_collection(params[:ips])
-    end
-    if @single_ip
-      validate_ip_address
-    elsif @ip_array
-      if valid_ip_array_length
-      else validate_ip_addresses
-      end
-    end
+    validate_ip_address if @single_ip
   end
 end
