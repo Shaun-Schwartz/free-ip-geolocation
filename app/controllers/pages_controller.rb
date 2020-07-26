0fa7a9ad593c520d986d7ba225c53ab069573025
Shaun Schwartz <me@shaunschwartz.com>
2020-07-26 15:16:38 -0700

    fix some bugs, clarify doc rate limiting wording

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authorized
 
   def home
-    # @request_ip = request.remote_ip
-    @request_ip = '193.37.253.69' # TODO: change before deploying
     if @request_ip == Constants::LOCALHOST_IPV4
       @location = Location.first(100).sample
     else
